91fa5fb0c4c1f4bb7c12801aaa53669cb22365b7
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-10-10 16:58:12 +0400

    fix

--- infra.php
+++ infra.php
@@ -5,56 +5,61 @@ use infrajs\excel\Xlsx;
 use infrajs\path\Path;
 use infrajs\each\Each;
 
-
+Path::reqif('~prices.php');
 Event::handler('Catalog.oninit', function (&$data) {
+	
 	$list = Prices::getList();
 	$ids = array();
-	foreach ($list as $dealer => $info) {
-		$ids[$dealer] = array();
-		$rule = Prices::getRule($dealer);
-		Event::$classes['Prices-'.$dealer] = function (&$obj) {
+	foreach ($list as $prod => $info) {
+		$ids[$prod] = array();
+		$rule = Prices::getRule($prod);
+		Event::$classes['Prices-'.$prod] = function (&$obj) {
 			return $obj['key'];
 		};
 
-		Xlsx::runPoss($info['data'], function &(&$pos, $i, $group) use (&$ids, $rule, $dealer) {
-			$key = Prices::getHash($pos, $rule['price'], $dealer);
-			//$id = $dealer.'-'.$key;
-			//$ids[$dealer][$id] = $pos;
+		Xlsx::runPoss($info['data'], function &(&$pos, $i, $group) use (&$ids, $rule, $prod) {
+			Prices::checkSynonyms($pos, $rule);
+			$key = Prices::getHash($pos, $rule['price'], $prod);
+			//$id = $prod.'-'.$key;
+			//$ids[$prod][$id] = $pos;
 			$pos['path'] = $group['path'];
 			$id = $key;
-			$ids[$dealer][$id] = $pos;
+			$ids[$prod][$id] = $pos;
 
 			$r = null;
 			return $r;
 		});
 		
 	};
+
 	Xlsx::runPoss($data, function &(&$pos) use ($ids) {
 		$r = null;
-		$dealer = $pos['producer'];
-		$rule = Prices::getRule($dealer);
-		$key = Prices::getHash($pos, $rule['catalog'], $dealer);
+		$prod = $pos['producer'];
+		$rule = Prices::getRule($prod);
+		$key = Prices::getHash($pos, $rule['catalog'], $prod);
 		$id = $key;
 
-		if (empty($ids[$dealer][$id])) return $r;
+		if (empty($ids[$prod][$id])) return $r;
 
-		$price = &$ids[$dealer][$id];
-
-		Prices::checkSynonyms($price, $rule);
-
+		$price = &$ids[$prod][$id];
 		$data = array(
 			'key' => $id,
 			'price' => $price,
 			'pos' => &$pos
 		);
 
-		Event::fire('Prices-'.$dealer.'.oninit', $data);
 		
-		if (isset($data['pos']['Цена']) && $data['pos']['Цена'] > Prices::$conf['costlimit']) {
-			unset($data['pos']['Цена']);
+
+		Event::fire('Prices-'.$prod.'.oninit', $data);
+		if (!isset($data['pos']['Цена'])){
+			if (isset($data['pos']['Цена оптовая'])) $data['pos']['Цена'] = $data['pos']['Цена оптовая'];
+			if (isset($data['pos']['Цена розничная'])) $data['pos']['Цена'] = $data['pos']['Цена розничная'];
 		}
+		
 		$r = null;
 		return $r;
 	});
+	//echo '<pre>';
+	//print_r($data);
 }, 'prices');
-Path::reqif('~prices.php');
+
